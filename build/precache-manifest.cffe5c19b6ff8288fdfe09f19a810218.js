3e53732651124593adaf51edef722e349e2280b6
chathuminav <chathuminav@hsenidmobile.com>
2022-01-30 03:28:38 +0530

    Heroku fix

---
+++ build/precache-manifest.cffe5c19b6ff8288fdfe09f19a810218.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ff97c8b9c55edbedb5454b7e8da5857",
+    "url": "/te/index.html"
+  },
+  {
+    "revision": "10daf072c522e6131e92",
+    "url": "/te/static/css/2.3445b3a4.chunk.css"
+  },
+  {
+    "revision": "f4ab33ef5aee01c8c19c",
+    "url": "/te/static/css/main.fe19b284.chunk.css"
+  },
+  {
+    "revision": "10daf072c522e6131e92",
+    "url": "/te/static/js/2.d594b230.chunk.js"
+  },
+  {
+    "revision": "97f1161acd0e6d97385ef7b190478233",
+    "url": "/te/static/js/2.d594b230.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4ab33ef5aee01c8c19c",
+    "url": "/te/static/js/main.8689594f.chunk.js"
+  },
+  {
+    "revision": "0aa185f2832e41558ee8",
+    "url": "/te/static/js/runtime-main.7e73c38a.js"
+  },
+  {
+    "revision": "c97c88957191814304e724a091c7445e",
+    "url": "/te/static/media/SLIDE_01.c97c8895.jpg"
+  },
+  {
+    "revision": "fabe6e1f121343b79d452079ffd2c780",
+    "url": "/te/static/media/SLIDE_02.fabe6e1f.jpg"
+  },
+  {
+    "revision": "835f9eb87244f171333a22eda891f0d3",
+    "url": "/te/static/media/SLIDE_03.835f9eb8.jpg"
+  },
+  {
+    "revision": "16ae00eebdb69e17f0f3afe2f80a2a4e",
+    "url": "/te/static/media/SLIDE_04.16ae00ee.jpg"
+  },
+  {
+    "revision": "e48324dc99e90c9e010736a2d6b05e6e",
+    "url": "/te/static/media/Te-bliss-mint.e48324dc.jpg"
+  },
+  {
+    "revision": "16ae00eebdb69e17f0f3afe2f80a2a4e",
+    "url": "/te/static/media/dark_tea.16ae00ee.jpg"
+  },
+  {
+    "revision": "a25c8f7162154368473055a9899f83d0",
+    "url": "/te/static/media/de.a25c8f71.png"
+  },
+  {
+    "revision": "a0f21f7bff7a0eb9ce4ebf9af7ef03c7",
+    "url": "/te/static/media/en.a0f21f7b.png"
+  },
+  {
+    "revision": "9c74404edd0092032d047c2bed3a59a9",
+    "url": "/te/static/media/estate_01_01.9c74404e.png"
+  },
+  {
+    "revision": "8698b2ea8fdbac2bdfb9bbdcefa4ecb5",
+    "url": "/te/static/media/estate_01_02.8698b2ea.png"
+  },
+  {
+    "revision": "a318b1f2618e887dc3acbc879777ab7a",
+    "url": "/te/static/media/estate_01_03.a318b1f2.png"
+  },
+  {
+    "revision": "61ccb76a11b0f79aca591bc1a5c06ec1",
+    "url": "/te/static/media/estate_01_04.61ccb76a.png"
+  },
+  {
+    "revision": "ac10e2a67ee8e4ec473f5bf808f909ed",
+    "url": "/te/static/media/garden_cover_full.ac10e2a6.jpg"
+  },
+  {
+    "revision": "58aa9361e4f9d22df7a0de23af360da7",
+    "url": "/te/static/media/garden_cover_mountains.58aa9361.png"
+  },
+  {
+    "revision": "13f3c5ab7a51d915dd663680a55c68c9",
+    "url": "/te/static/media/item_01.13f3c5ab.png"
+  },
+  {
+    "revision": "852588f121a3a90d83490c8678604e59",
+    "url": "/te/static/media/loader.852588f1.gif"
+  },
+  {
+    "revision": "414c1bac5b29987c5c7acd158144ee65",
+    "url": "/te/static/media/logo_400px.414c1bac.png"
+  },
+  {
+    "revision": "6db06effa31154fd146de5f5df47866a",
+    "url": "/te/static/media/logo_header.6db06eff.png"
+  },
+  {
+    "revision": "1d0e2ae1ca948b7fc5c700537df6bce1",
+    "url": "/te/static/media/paradise-regional.1d0e2ae1.png"
+  },
+  {
+    "revision": "9c74404edd0092032d047c2bed3a59a9",
+    "url": "/te/static/media/product_01.9c74404e.png"
+  },
+  {
+    "revision": "2f8f8680153aa4d46a4921319a47c7fe",
+    "url": "/te/static/media/product_02.2f8f8680.jpg"
+  },
+  {
+    "revision": "e9ad4e41520b46ed75731f014b696fbb",
+    "url": "/te/static/media/product_03.e9ad4e41.jpg"
+  },
+  {
+    "revision": "ed3edc8bb64a7b3287aad9d94f652e43",
+    "url": "/te/static/media/region_01_01.ed3edc8b.png"
+  },
+  {
+    "revision": "8698b2ea8fdbac2bdfb9bbdcefa4ecb5",
+    "url": "/te/static/media/region_01_02.8698b2ea.png"
+  },
+  {
+    "revision": "a318b1f2618e887dc3acbc879777ab7a",
+    "url": "/te/static/media/region_01_03.a318b1f2.png"
+  },
+  {
+    "revision": "61ccb76a11b0f79aca591bc1a5c06ec1",
+    "url": "/te/static/media/region_01_04.61ccb76a.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/te/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/te/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/te/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/te/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "5dd039a1916040ecc8fbf5f2b642f6cf",
+    "url": "/te/static/media/slider-leafs.5dd039a1.png"
+  },
+  {
+    "revision": "c6446b82978959df7e574825faac1377",
+    "url": "/te/static/media/te_garden_slide_show.c6446b82.jpg"
+  },
+  {
+    "revision": "a12fddaffaf26fd6376481de0e3799de",
+    "url": "/te/static/media/tea-leaves.a12fddaf.jpg"
+  }
+]);
